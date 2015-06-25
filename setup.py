cf60c5e75c2690c44ad6625e93d7b868202f1638
Keiichi Shima <keiichi@iijlab.net>
2015-06-25 19:38:38 +0900

    Version 0.1.3
    Now this package requires gsq>=0.1.5.

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ except ImportError:
     read_md = lambda f: open(f, 'r').read()
 
 setup(name='pcalg',
-      version='0.1.2',
+      version='0.1.3',
       description='CPDAG Estimation using PC-Algorithm',
       long_description=read_md('README.md'),
       author='Keiichi SHIMA',
       author_email='keiichi@iijlab.net',
       py_modules=['pcalg'],
-      install_requires=['networkx>=1.9.1', 'gsq>=0.1.1'],
+      install_requires=['networkx>=1.9.1', 'gsq>=0.1.5'],
       classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
