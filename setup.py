f270e2bdb76b88c8f80a1ea07317ff4be88e2359
Keiichi SHIMA <keiichi@iijlab.net>
2019-02-05 14:16:04 +0900

    increased the patchlevel

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
     read_md = lambda f: open(f, 'r').read()
 
 setup(name='pcalg',
-      version='0.1.8',
+      version='0.1.9',
       description='CPDAG Estimation using PC-Algorithm',
       long_description=read_md('README.md'),
       author='Keiichi SHIMA',
